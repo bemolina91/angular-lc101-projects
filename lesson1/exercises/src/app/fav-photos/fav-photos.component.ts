8c0d38b426ce5154b7c87fd2e0c685eef5e4f776
bemolina91 <bemolina9@icloud.com>
2023-02-27 14:09:13 -0600

    add css styling and images

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'PHOTOS';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://www.dropbox.com/s/6ollksmnhgspdm9/IMG_0764.jpeg?raw=1';
+  image3 = 'https://www.dropbox.com/s/6vjm29fcjdun8cc/IMG_6366.JPG?raw=1';
 
   constructor() { }
 
