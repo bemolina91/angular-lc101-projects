20c9d22154d18a210f39fa123b496882a1053ffe
bemolina91 <bemolina9@icloud.com>
2023-02-27 15:20:40 -0600

    completed exercise assignment

---
+++ lesson1/exercises/src/app/fav-links/fav-links.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fav-links',
+  templateUrl: './fav-links.component.html',
+  styleUrls: ['./fav-links.component.css']
+})
+export class FavLinksComponent implements OnInit {
+  favLinksTitle = 'Links';
+  favLinks = ['https://www.launchcode.org', 'https://www.w3schools.com'];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'PHOTOS';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+  photosTitle = 'Photos/Images';
+  image1 = 'https://www.dropbox.com/s/rbz68dbkx4l1ai4/IMG_4673.jpeg?raw=1';
   image2 = 'https://www.dropbox.com/s/6ollksmnhgspdm9/IMG_0764.jpeg?raw=1';
   image3 = 'https://www.dropbox.com/s/6vjm29fcjdun8cc/IMG_6366.JPG?raw=1';
 
